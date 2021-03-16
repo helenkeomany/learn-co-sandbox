5fb2a463be9c5dd23fcb842598a318faa6e96eb4
Helen Keomany <helenkeomany@gmail.com>
2021-03-16 23:02:27 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,15 @@
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+puts stylish_chef
+
+def love
+  boyfriend = "Avrohom"
+  return "Shmuel"
+  "Guy Fieri"
+end
+
+puts love
